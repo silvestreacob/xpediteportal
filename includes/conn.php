2fdd153b0b5042e3b2196396c336f4cd732eed66
hackcob <68264634+hackcob@users.noreply.github.com>
2020-07-14 14:55:05 +0800

    Update conn.php

--- includes/conn.php
+++ includes/conn.php
@@ -1,7 +1,7 @@
 <?php
 
-	$conn = new mysqli('localhost', 'root', '', 'datax');
-	//$conn = new mysqli('fdb18.biz.nf', '3319611_datax', 'F8aiEkQ#s*CJ2ir', '3319611_datax');
+	//$conn = new mysqli('localhost', 'root', '', 'datax');
+	$conn = new mysqli('fdb18.biz.nf', '3319611_datax', 'F8aiEkQ#s*CJ2ir', '3319611_datax');
 		
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
