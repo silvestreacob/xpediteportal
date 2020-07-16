2a1ed67c81b2a33bc0ff471f6c79bf08cce66396
hackcob <68264634+hackcob@users.noreply.github.com>
2020-07-16 08:32:36 +0800

    update connection config

--- includes/conn.php
+++ includes/conn.php
@@ -2,6 +2,15 @@
 
 	$conn = new mysqli('localhost', 'root', '', 'datax');
 	//$conn = new mysqli('fdb18.biz.nf', '3319611_datax', 'F8aiEkQ#s*CJ2ir', '3319611_datax');
+
+	$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+	$server = $url["host"];
+	$username = $url["user"];
+	$password = $url["pass"];
+	$db = substr($url["path"], 1);
+
+	$conn = new mysqli($server, $username, $password, $db);
 		
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
